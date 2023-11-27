86605f86dec4de7086a9e8cbb2367a9c9e6b2828
Will Colding <william.colding@gmail.com>
2023-11-26 22:18:29 -0500

    SWE1R: Exclude pit droid checks if disabled

--- worlds/swe1r/__init__.py
+++ worlds/swe1r/__init__.py
@@ -85,6 +85,9 @@ class SWRWorld(World):
     def create_regions(self):
         region_menu = Region("Menu", self.player, self.multiworld)
         for loc in location_table:
+            if "Pit Droid Shop" in loc:
+                if (self.multiworld.disable_part_degradation[self.player].value):
+                   continue 
             region_menu.locations.append(SWRacerLocation(self.player, loc, self.location_name_to_id[loc], region_menu))
         self.multiworld.regions.append(region_menu)
 
